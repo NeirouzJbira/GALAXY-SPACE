508a673105e2bcf3094c1f9c284b16bcd1984511
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-01-13 22:10:19 +0100

    player validator password

--- FRONT-END/src/app/register/register.component.ts
+++ FRONT-END/src/app/register/register.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
-@Component({
+import { AbstractControl, FormGroup, FormControl, Validators } from '@angular/forms';@Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
-
+  myForm: FormGroup;
+  constructor() { 
+  
+    this.myForm = new FormGroup({
+      email: new FormControl(null, Validators.email),
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+      checkpassword: new FormControl(null, this.passValidator)
+    })
+}
   ngOnInit(): void {
   }
+  passValidator(control: AbstractControl) {
+    if (control && (control.value !== null || control.value !== undefined)) {
+      const cnfpassValue = control.value;
 
+      const passControl = control.root.get('password');
+      if (passControl) {
+        const passValue = passControl.value;
+        if (passValue !== cnfpassValue || passValue === '') {
+          return {
+            isError: true
+          };
+        }
+      }
+    }
+
+    return null;
+  }
 }
