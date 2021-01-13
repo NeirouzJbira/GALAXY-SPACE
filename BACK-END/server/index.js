bf0ca583dfcb18379e8d2ad08231884f30c8db4d
malekaridhi <malek.aridhii@gmail.com>
2021-01-13 10:32:32 +0100

    init

--- BACK-END/server/index.js
+++ BACK-END/server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose= require("mongoose");
+var crypto = require('crypto');
+var nodemailer = require('nodemailer');
 const imageModule = require ('./database/image')
 require("dotenv").config();
 
@@ -15,7 +17,22 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
     console.log("MONGO connected")
 }
 );
+/*post/login*/
+/*res.send({ token: generateToken(user), user: user.toJSON() });*/
+/* */
+/*// Create a verification token for this user
+        var token = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
 
+        // Save the verification token
+        token.save(function (err) {
+            if (err) { return res.status(500).send({ msg: err.message }); }
+
+            // Send the email
+            var transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: process.env.SENDGRID_USERNAME, pass: process.env.SENDGRID_PASSWORD } });
+            var mailOptions = { from: 'no-reply@yourwebapplication.com', to: user.email, subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n' };
+            transporter.sendMail(mailOptions, function (err) {
+                if (err) { return res.status(500).send({ msg: err.message }); }
+                res.status(200).send('A verification email has been sent to ' + user.email + '.'); */
 app.use (express.static('images'))
 
 const PORT = process.env.PORT || 4000;
