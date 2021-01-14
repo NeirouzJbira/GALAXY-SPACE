4f26b48072ca322c7b8e44af29de39c7f7ae043e
DhiaMbarki <tahridhia22@gmail.com>
2021-01-14 14:19:33 +0100

    random options

--- FRONT-END/games-app/src/app/app.component.ts
+++ FRONT-END/games-app/src/app/app.component.ts
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public play(action: string): void { //so here when you click any of the buttons it will work in the console
     console.log(action);
+    this.computer()
+  }
+    private computer(): void {
+      const randomNumber = Math.floor(Math.random() * 3);
+      const options : string[] = ['rock', 'paper', 'scissors'] ; // and here when you click like on button paper the computer rondomly he will choose  another option with you
+      console.log(options[randomNumber])
+    
   }
 }
