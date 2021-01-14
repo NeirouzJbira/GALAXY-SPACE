fd20e7f4f7fc5c0bcd1e4d496778e4056fac37f6
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-01-14 21:59:23 +0100

    fixing frint end files

--- FRONT-END/src/app/app.module.ts
+++ FRONT-END/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-
-import { FormsModule,  ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RegisterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
