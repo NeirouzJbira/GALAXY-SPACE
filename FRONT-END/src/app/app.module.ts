f8abe53ddd719c38c2c068202031c2137649d774
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-01-15 10:15:33 +0100

    init

--- FRONT-END/src/app/app.module.ts
+++ FRONT-END/src/app/app.module.ts
@@ -52,9 +52,6 @@ const appRoutes: Routes =  [
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    
-    
-    
   ],
   providers: [ValidateService,AuthService],
   bootstrap: [AppComponent]
